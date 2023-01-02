5848904b2bd91a59fe4dcbf99a71fe71ded6a431
ali rahimi <alirahimi818@gmail.com>
2023-01-02 21:43:52 +0100

    check setting data when plugin register

--- index.php
+++ index.php
@@ -22,10 +22,10 @@ require_once(PLUGIN_BASE_URL . 'includes/setting.php');
 function run_default_setting()
 {
     update_option('qr_bonus_checksum', uniqid() . '--1');
-    update_option('qr_bonus_win_count', '12');
-    update_option('qr_bonus_date_format', 'D. d.m.Y H:i');
-    update_option('qr_bonus_card_deactivate_img_url', plugins_url('/assets/coffee.jpg', PLUGIN_FILE_URL));
-    update_option('qr_bonus_card_active_img_url', plugins_url('/assets/coffee-active.jpg', PLUGIN_FILE_URL));
+    update_option('qr_bonus_win_count', get_option('qr_bonus_win_count') ?? '12');
+    update_option('qr_bonus_date_format', get_option('qr_bonus_date_format') ?? 'D. d.m.Y H:i');
+    update_option('qr_bonus_card_deactivate_img_url',get_option('qr_bonus_card_deactivate_img_url') ??  plugins_url('/assets/coffee.jpg', PLUGIN_FILE_URL));
+    update_option('qr_bonus_card_active_img_url', get_option('qr_bonus_card_active_img_url') ?? plugins_url('/assets/coffee-active.jpg', PLUGIN_FILE_URL));
 }
 
 register_activation_hook(PLUGIN_FILE_URL, 'run_default_setting');
